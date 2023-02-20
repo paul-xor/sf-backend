aa2c0950c2bd50e659df69ceda88e6bfca888d66
Pasha <psmolov@nautilusdigital.com>
2023-02-20 18:56:11 -0500

    s3: cdk Node Lambda

--- infra/SpaceStack.ts
+++ infra/SpaceStack.ts
@@ -4,6 +4,7 @@ import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambd
 import { join } from 'path';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { GenericTable } from './GenericTable';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class SpaceStack extends Stack {
   private api : RestApi = new RestApi(this, 'SpaceApi')
@@ -20,6 +21,11 @@ export class SpaceStack extends Stack {
       runtime: Runtime.NODEJS_18_X,
       code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
       handler: 'hello.main'
+    })
+
+    const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
+      entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
+      handler: 'handler'
     })
 
     // Hello Lambda integration
