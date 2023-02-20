efb98a9edc94d2055d984c65423af32883d66013
Pasha <psmolov@nautilusdigital.com>
2023-02-19 20:27:17 -0500

    s3: aws dynamodb with cdk

--- infra/SpaceStack.ts
+++ infra/SpaceStack.ts
@@ -3,9 +3,15 @@ import { Construct } from 'constructs';
 import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { GenericTable } from './GenericTable';
 
 export class SpaceStack extends Stack {
   private api : RestApi = new RestApi(this, 'SpaceApi')
+  private spacesTable: GenericTable = new GenericTable(
+    'SpacesTable',
+    'spaceId',
+    this
+  )
 
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props)
